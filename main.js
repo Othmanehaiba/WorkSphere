fb7f0471a49acf3e251fdce6638a181efe10da0d
Othmane haiba <othmanehaiba4@gmail.com>
2025-11-19 11:05:23 +0100

    initialising the variables

---
+++ main.js
@@ -0,0 +1,20 @@
+const btnAddWorker = document.getElementById("addWorker");
+const pop_up = document.querySelector(".pop_up");
+const btnAddExperience = document.getElementById("experience");
+const closePopUp = document.getElementById("X");
+const exp = document.querySelector(".Exp");
+let Employe = {
+  FullName: "",
+  role: "",
+  picURL: "",
+  email: "",
+  phone: 0,
+  experiences: [{PPost: "",
+                PEntreprise: "",
+                SDate: 0,
+                Edate: 0}]
+};
+
+
+
+
